fa846306b51c77488d7c7c4aefa9b281a65b088a
Ian <mus5002@aol.com>
2019-08-27 21:08:24 -0400

    added calendar to change the date and fetch data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 
 function App() {
+  const onMount = 'mount'
   const [data, setData] = useState({
     title: '',
     url: '',
@@ -21,12 +22,24 @@ function App() {
         console.log(err)
         return err
       })
-  }, [])
+  }, [onMount])
 
   console.log(data)
 
+  const changeDate = (e) => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=j53v9dvTGZXp3UeeJV9KgkvXtf35lawivkMwfwtS&date=${e.target.value}`)
+      .then(res => {
+        return setData(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+        return err
+      })
+  }
+
   return (
     <div className="App">
+      <input type='date' value={data.date} onChange={(e) => changeDate(e)}/>
       <h1 className='tile'>{data.title}</h1>
       <img className='image' src={data.url} alt='nasa pic of day' />
       <p className='explanation'>{data.explanation}</p>
