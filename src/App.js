6e753437e0fde3c3ed0ee4a86e8ebb563d4a6f41
Ian <mus5002@aol.com>
2019-08-29 19:58:36 -0400

    added styled components, added useEffect for changing date

--- src/App.js
+++ src/App.js
@@ -25,24 +25,33 @@ function App() {
       })
   }, [onMount])
 
-  console.log(data)
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data.date}`)
+    .then(res => {
+      return setData(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+      return err
+    })
+  }, [data.date])
 
-  const changeDate = (e) => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${e.target.value}`)
-      .then(res => {
-        return setData(res.data)
-      })
-      .catch(err => {
-        console.log(err)
-        return err
-      })
-  }
+  // const changeDate = (e) => {
+  //   axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${e.target.value}`)
+  //     .then(res => {
+  //       return setData(res.data)
+  //     })
+  //     .catch(err => {
+  //       console.log(err)
+  //       return err
+  //     })
+  // }
 
   return (
     <div className="App">
-      <input type='date' value={data.date} onChange={(e) => changeDate(e)}/>
+      <input type='date' value={data.date} onChange={(e) => setData({date: e.target.value})}/>
       <h1 className='tile'>{data.title}</h1>
-      { (data.media_type === 'image') ? <img className='image' src={data.url} alt='nasa pic of day' /> : <iframe src={data.url}></iframe>}
+      { (data.media_type === 'image') ? <img className='image' src={data.url} alt='nasa pic of day' /> : <iframe title='video' src={data.url}></iframe> }
       <p className='explanation'>{data.explanation}</p>
       <h4 className='copyright'>{data.copyright}</h4>
       <p className='date'>{data.date}</p>
